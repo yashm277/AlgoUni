853c33dc1341b3f4e9d3350c90e720e60ae8b478
yashm277 <f20210001@pilani.bits-pilani.ac.in>
2023-07-09 21:11:45 +0530

    work complete for 9 July

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a project that aims to create an online judge.
+More details here:
+https://docs.google.com/document/d/1tuZV9_EuvGjCXP9zPeEbpGkL3-83dOraJAp8znyMh8A/edit
--- server/models/submission.js
+++ server/models/submission.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-//need to create a schema called submissionSchema which will have a foreign key to both the user and problem schema, a code field which is a string and a result field which is a string enum of either "approved","rejected","TLE","MLE","WA","RE"
+//need to create a schema called submissionSchema which will have a foreign key to both the user and problem schema, a code field which is a string and a result field which is a string enum of either "approved","rejected","TLE","MLE","WA","RE" //also needs to have a date field which is a date type and is required and has a default value of Date.now
 const submissionSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     problem: { type: mongoose.Schema.Types.ObjectId, ref: 'Problem', required: true},
     code: {type: String, required: true},
-    result: {type: String, required: true, enum: ["approved","rejected","TLE","MLE","WA","RE"]}
+    result: {type: String, required: true, enum: ["approved","rejected","TLE","MLE","WA","RE"]},
+    date: {type: Date, required: true, default: Date.now}
 });
 
 const Submission = mongoose.model("Submission", submissionSchema);
